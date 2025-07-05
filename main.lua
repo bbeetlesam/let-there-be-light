83c8a792057de3619699232c199e9e1b3aa254b0
bbeetlesam <samsismines@gmail.com>
2025-07-05 21:03:50 +0700

    refactor: remove unnecessary codes
    moved to maingame.lua
    
    FUCKING HELL MY CODE IS SO CLEAN JUST LIKE MY LIFE (I HOPE)

--- main.lua
+++ main.lua
@@ -1,77 +1,28 @@
-local const = require("src.const")
-local CanvasManager = require("src.canvas")
-local utils = require("src.utils")
-local Player = require("src.player")
-local Camera = require("src.camera")
+local sceneManager = require("src.scenes.sceneManager")
 local shaders = require("src.shaders.shaders")
-local Font = require("src.font")
 
 function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest")
+    love.mouse.setVisible(false)
 
-    image = love.graphics.newImage("dump/octocat-1747987801490.png")
-    lightRadius = 500
-
-    player = Player:new(const.GAME_WIDTH/2, const.GAME_HEIGHT/2)
-    camera = Camera:new(0, 0, 1)
-
-    canvases = CanvasManager:new()
-    canvases:create("main", const.GAME_WIDTH, const.GAME_HEIGHT, const.SCALE_FACTOR, {"nearest", "nearest"})
-    -- canvases:addShader("main", shaders.light)
     shaders.load()
-
-    text = Font:new({"assets/img/sprite-font2.png", "assets/img/sprite-font1.png"}, {12, 12}, {16, 16}, 16, 6, 10)
+    sceneManager:load("intro")
 end
 
 function love.update(dt)
-    if love.keyboard.isDown("up") and lightRadius < 1500 then
-        lightRadius = lightRadius + 900 * dt
-    elseif love.keyboard.isDown("down") and lightRadius > 100 then
-        lightRadius = lightRadius - 900 * dt
-    end
-
-    player:update(dt)
-    local x, y = player:getPosition()
-    camera:setPosition(x, y)
-
-    local a, b = utils.screenToWorld(const.GAME_WIDTH/2, const.GAME_HEIGHT/2 - 10)
-    shaders.light:send("lightPos", {a, b})
-    shaders.light:send("radius", lightRadius)
-    text:update(dt)
+    sceneManager:update(dt)
 end
 
 function love.draw()
-    canvases:with("main", function()
-        love.graphics.setColor(love.math.colorFromBytes(17, 17, 17))
-        love.graphics.rectangle("fill", 0, 0, const.GAME_WIDTH, const.GAME_HEIGHT)
-
-        camera:attach()
-            love.graphics.setColor(1, 1, 1)
-            love.graphics.draw(image, const.GAME_WIDTH/2, const.GAME_HEIGHT/2, 0, 1/const.SCALE_FACTOR/2, 1/const.SCALE_FACTOR/2, image:getWidth()/2, image:getHeight()/2)
-
-            love.graphics.setColor(1, 1, 0)
-            player:draw()
-
-            love.graphics.setColor(0, 1, 0)
-            love.graphics.points(0, 0)
-        camera:detach()
-    end)
-
-    love.graphics.setShader(shaders.light)
-    canvases:drawAll()
-    love.graphics.setShader()
-
-    local x, y = player:getPosition()
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.print("Light: " .. lightRadius, 10, 10)
-    love.graphics.print("X " .. x .. "\nY " .. y, 10, 40)
-    text:print("let there. be light.. maro?", const.SCREEN_WIDTH/2, const.SCREEN_HEIGHT*9/10, 2, 0.5, 0.5)
+    sceneManager:draw()
 end
 
 function love.keyreleased(key, _, _)
+    sceneManager:keypressed(key)
+
     if key == "escape" then
         love.event.quit()
-    elseif key == "f11" then 
+    elseif key == "f11" then
     end
 end
 
